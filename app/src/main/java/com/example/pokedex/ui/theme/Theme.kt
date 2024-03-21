6f16cb0a5c27127d5d0c21f1d7d0c6b827d9957e
Anu-Basu <119494632+Anu-Basu@users.noreply.github.com>
2024-03-21 17:38:01 +0530

    adding the shapes, theme and other ui elements

--- app/src/main/java/com/example/pokedex/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pokedex/ui/theme/Theme.kt
@@ -2,8 +2,6 @@ package com.example.pokedex.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
