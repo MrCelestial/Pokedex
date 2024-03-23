7e2a634e57b1f5fafecdaa7c076d1d3ee309e68a
Anu-Basu <119494632+Anu-Basu@users.noreply.github.com>
2024-03-23 20:06:36 +0530

    added new files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation ("androidx.compose.ui:ui:$compose_version")
     implementation ("androidx.compose.material:material:$compose_version")
     implementation ("androidx.compose.ui:ui-tooling:$compose_version")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation ("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3-android:1.2.1")
     testImplementation ("junit:junit:4.13.2")
@@ -72,7 +71,7 @@ dependencies {
 
     implementation ("androidx.compose.material:material-icons-core:$compose_version")
     implementation ("androidx.compose.material:material-icons-extended:$compose_version")
-    implementation ("androidx.lifecycle:lifecycle-view-model-compose:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation ("androidx.navigation:navigation-compose:2.7.7")
     implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
@@ -84,12 +83,13 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:okhttp:4.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
     // Timber
     implementation ("com.jakewharton.timber:timber:4.7.1")
 
     // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // Coroutine Lifecycle Scopes
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
