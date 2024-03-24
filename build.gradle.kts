a7c4b02524a8828ccd6c4e826af4efadd57b9be0
Anu-Basu <119494632+Anu-Basu@users.noreply.github.com>
2024-03-24 13:09:35 +0530

    added new files

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 plugins {
     id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
     id ("org.jetbrains.kotlin.jvm") version "1.9.20" apply false
-    id ("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
 }
