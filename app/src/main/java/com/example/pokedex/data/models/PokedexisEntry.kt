5d489b949a25ff87a11e6732a29ccc5eaebd87b8
Anu-Basu <119494632+Anu-Basu@users.noreply.github.com>
2024-03-25 20:30:00 +0530

    added new files

---
+++ app/src/main/java/com/example/pokedex/data/models/PokedexisEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pokedex.data.models
+
+import androidx.navigation.NavController
+
+data class PokedexisEntry (
+    val pokemonName : String,
+    val imageUrl: String,
+    val number: Int
+)
